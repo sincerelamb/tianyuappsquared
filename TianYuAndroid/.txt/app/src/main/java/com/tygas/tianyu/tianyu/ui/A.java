2962a340844aecad07a7584ac6a0370318a49b7e
sincerelamb <896201545@qq.com>
2016-08-15 17:24:36 +0800

    160815 1st

--- TianYuAndroid/.txt/app/src/main/java/com/tygas/tianyu/tianyu/ui/A.java
+++
@@ -1,7 +0,0 @@
-package com.tygas.tianyu.tianyu.ui;
-
-/**
- * Created by Administrator on 2016/1/18.
- */
-public class A {
-}
--- TianYuAndroid/.txt/app/src/main/java/com/tygas/tianyu/tianyu/ui/view/B.java
+++
@@ -1,7 +0,0 @@
-package com.tygas.tianyu.tianyu.ui.view;
-
-/**
- * Created by Administrator on 2016/1/19.
- */
-public class B {
-}
